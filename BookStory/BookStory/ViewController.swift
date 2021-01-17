07159cb9b2fe28ad1a56dd6ff9de8fb30d60a307
kimhyebeen <kimhbin@naver.com>
2021-01-17 21:02:50 +0900

    #1. Create a project for BookStory

---
+++ BookStory/BookStory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookStory
+//
+//  Created by 김혜빈 on 2021/01/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BookStory/BookStoryTests/BookStoryTests.swift
@@ -0,0 +1,33 @@
+//
+//  BookStoryTests.swift
+//  BookStoryTests
+//
+//  Created by 김혜빈 on 2021/01/17.
+//
+
+import XCTest
+@testable import BookStory
+
+class BookStoryTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

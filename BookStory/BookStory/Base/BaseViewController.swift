8e67c577aedd8d0d9d9184e1ddf054628505ec19
kimhyebeen <kimhbin@naver.com>
2021-01-18 00:47:03 +0900

    #1. Create a show toast message function in BaseViewController

--- BookStory/BookStory/Base/BaseViewController.swift
+++ BookStory/BookStory/Base/BaseViewController.swift
@@ -9,5 +9,44 @@ import UIKit
 import Then
 
 class BaseViewController: UIViewController {
-    
+    func showToast(view: UIView, message : String, color: UIColor = .black, font: UIFont = UIFont.systemFont(ofSize: 14.0)) {
+        OperationQueue.main.addOperation {
+            let toastLabel = self.getToastLabel(view: view, message, color, font)
+            view.addSubview(toastLabel)
+            UIView.animate(
+                withDuration: 2.0,
+                delay: 1.5,
+                options: .curveEaseOut,
+                animations: { toastLabel.alpha = 0.0 },
+                completion: {(isCompleted) in toastLabel.removeFromSuperview() }
+            )
+        }
+    }
+
+    private func getToastLabel(view: UIView,_ message: String,_ color: UIColor,_ font: UIFont) -> UILabel {
+        var width: CGFloat = CGFloat(message.count * 8 + 40)
+        if width > self.view.frame.width * 0.8 {
+            width = self.view.frame.width * 0.8
+        }
+        
+        let toastLabel = UILabel(
+            frame: CGRect(
+                x: view.frame.size.width/2 - width/2,
+                y: view.frame.size.height-100,
+                width: width,
+                height: 35
+            )
+        ).then { label in
+            label.backgroundColor = color.withAlphaComponent(0.6)
+            label.textColor = UIColor.white
+            label.font = font
+            label.textAlignment = .center
+            label.text = message
+            label.alpha = 1.0
+            label.layer.cornerRadius = 15;
+            label.clipsToBounds = true
+        }
+        
+        return toastLabel
+    }
 }

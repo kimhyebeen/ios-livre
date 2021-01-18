e37318abf3813334237c2ea70c4834f27f46ef05
kimhyebeen <kimhbin@naver.com>
2021-01-18 22:46:46 +0900

    #3. Create a method for gradient

---
+++ BookStory/BookStory/View/ViewExtension.swift
@@ -0,0 +1,19 @@
+//
+//  ViewExtension.swift
+//  BookStory
+//
+//  Created by 김혜빈 on 2021/01/18.
+//
+
+import UIKit
+
+extension UIView {
+    func setGradient(colors: [CGColor], sx: CGFloat = 1, sy: CGFloat = 0, ex: CGFloat = 0, ey: CGFloat = 1) {
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.colors = colors
+        gradient.frame = bounds
+        gradient.startPoint = CGPoint(x: sx, y: sy)
+        gradient.endPoint = CGPoint(x: ex, y: ey)
+        layer.addSublayer(gradient)
+    }
+}

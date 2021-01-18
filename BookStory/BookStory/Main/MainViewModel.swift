f9d7d98e6550bcfec842d53c971ac7a4bf723a31
kimhyebeen <kimhbin@naver.com>
2021-01-18 15:17:56 +0900

    #4. Create a MainViewModel

---
+++ BookStory/BookStory/Main/MainViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewModel.swift
+//  BookStory
+//
+//  Created by 김혜빈 on 2021/01/18.
+//
+
+import RxSwift
+
+class MainViewModel {
+    let input = Input()
+    let output = Output()
+    let disposeBag = DisposeBag()
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+}

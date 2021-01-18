3f7094d559a0f2937ee0d16c741f2ec9dfe1cb3e
kimhyebeen <kimhbin@naver.com>
2021-01-18 14:30:32 +0900

    #2. Create a method for request news

---
+++ BookStory/BookStory/Data/Requests/RequestNews.swift
@@ -0,0 +1,25 @@
+//
+//  RequestNews.swift
+//  BookStory
+//
+//  Created by 김혜빈 on 2021/01/18.
+//
+
+import Alamofire
+import RxSwift
+
+func requestNews(query: String, start: Int = 1, display: Int = 10) {
+    let url = URLConfig.news
+    
+    AF.request(url,
+       method: .get,
+       parameters: ["query" : query, "start" : start, "display" : display],
+       encoding: URLEncoding.default,
+       headers: ["X-Naver-Client-Id":SecretKeySet.naverClientId, "X-Naver-Client-Secret":SecretKeySet.naverClientSecret])
+        .validate(statusCode: 200..<300)
+        .responseJSON { (json) in
+            guard let data = json.data else { return }
+            let response = try? JSONDecoder().decode(NewsSearchResponse.self, from: data)
+            // todo - response 데이터 활용
+    }
+}

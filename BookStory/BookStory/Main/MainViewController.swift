e7a2f7f3bcf1a14b517d670aba6540db006360bf
kimhyebeen <kimhbin@naver.com>
2021-01-18 23:51:53 +0900

    #3. Lottie 에니메이션 아이콘 추가

--- BookStory/BookStory/Main/MainViewController.swift
+++ BookStory/BookStory/Main/MainViewController.swift
@@ -6,8 +6,10 @@
 //
 
 import UIKit
+import Lottie
 
 class MainViewController: UIViewController {
+    let animationView = AnimationView(name: "main-book")
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +19,21 @@ class MainViewController: UIViewController {
 
     func setupView() {
         self.view.setGradient(colors: [UIColor(named: "gradient_start")!.cgColor, UIColor(named: "gradient_end")!.cgColor])
+        
+        setupAnimationView()
+    }
+    
+    func setupAnimationView() {
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        self.view.addSubview(animationView) // 애니메이션뷰를 메인뷰에 추가
+        
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        animationView.widthAnchor.constraint(equalToConstant: 170).isActive = true
+        animationView.heightAnchor.constraint(equalToConstant: 170).isActive = true
+        animationView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        animationView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: self.view.frame.height * 0.15).isActive = true
+        animationView.play() // 애미메이션뷰 실행
     }
 
 }

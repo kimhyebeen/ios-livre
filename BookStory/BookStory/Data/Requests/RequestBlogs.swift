ec03e36d0f0128f827b29713b90cf842a6acfc96
kimhyebeen <kimhbin@naver.com>
2021-01-19 19:04:54 +0900

    #7. 검색 요청 메서드 코드 정리

--- BookStory/BookStory/Data/Requests/RequestBlogs.swift
+++ BookStory/BookStory/Data/Requests/RequestBlogs.swift
@@ -15,25 +15,26 @@ func requestBlogs<T>(query: String, start: Int = 1, display: Int = 10, relay: Pu
        method: .get,
        parameters: ["query" : query, "start" : start, "display" : display],
        encoding: URLEncoding.default,
-       headers: ["X-Naver-Client-Id":SecretKeySet.naverClientId, "X-Naver-Client-Secret":SecretKeySet.naverClientSecret])
-        .validate(statusCode: 200..<300)
-        .responseJSON { (json) in
-            guard let data = json.data else { return }
-            let response = try? JSONDecoder().decode(BlogSearchResponse.self, from: data)
-            
-            guard let items = response?.items else { return }
-            if T.self == [BlogItem].self {
-                relay.accept(
-                    items.map { item in
-                        BlogItem(
-                            title: item.title,
-                            link: item.link,
-                            description: item.description,
-                            bloggername: item.bloggername,
-                            postDate: item.postDate
-                        )
-                    } as! T
-                )
-            } else { print("request blog search - 왜 다 타입이 안맞을까???") }
+       headers: ["X-Naver-Client-Id":SecretKeySet.naverClientId, "X-Naver-Client-Secret":SecretKeySet.naverClientSecret]
+    )
+    .validate(statusCode: 200..<300)
+    .responseJSON { (json) in
+        guard let data = json.data else { return }
+        let response = try? JSONDecoder().decode(BlogSearchResponse.self, from: data)
+        
+        guard let items = response?.items else { return }
+        if T.self == [BlogItem].self {
+            relay.accept(
+                items.map { item in
+                    BlogItem(
+                        title: item.title,
+                        link: item.link,
+                        description: item.description,
+                        bloggername: item.bloggername,
+                        postDate: item.postDate
+                    )
+                } as! T
+            )
+        } else { print("request blogs - 왜 타입이 안맞을까???") }
     }
 }
--- BookStory/BookStory/Data/Requests/RequestBookSearch.swift
+++ BookStory/BookStory/Data/Requests/RequestBookSearch.swift
@@ -15,27 +15,28 @@ func requestBookSearch<T>(query: String, start: Int = 1, display: Int = 10, rela
        method: .get,
        parameters: ["query" : query, "start" : start, "display" : display],
        encoding: URLEncoding.default,
-       headers: ["X-Naver-Client-Id":SecretKeySet.naverClientId, "X-Naver-Client-Secret":SecretKeySet.naverClientSecret])
-        .validate(statusCode: 200..<300)
-        .responseJSON { (json) in
-            guard let data = json.data else { return }
-            let response = try? JSONDecoder().decode(BookSearchResponse.self, from: data)
-            
-            guard let items = response?.items else { return }
-            if T.self == [SimpleBookItem].self {
-                relay.accept(
-                    items.map { item in
-                        SimpleBookItem(
-                            title: item.title,
-                            image: item.image,
-                            author: item.author,
-                            publishDate: item.publishDate,
-                            isbn: item.isbn
-                        )
-                    } as! T
-                )
-            } else if T.self == [BookSearchItem].self {
-                // todo - BookDetailInfoViewController
-            } else { print("request book search - 왜 다 타입이 안맞을까???") }
+       headers: ["X-Naver-Client-Id":SecretKeySet.naverClientId, "X-Naver-Client-Secret":SecretKeySet.naverClientSecret]
+    )
+    .validate(statusCode: 200..<300)
+    .responseJSON { (json) in
+        guard let data = json.data else { return }
+        let response = try? JSONDecoder().decode(BookSearchResponse.self, from: data)
+        
+        guard let items = response?.items else { return }
+        if T.self == [SimpleBookItem].self {
+            relay.accept(
+                items.map { item in
+                    SimpleBookItem(
+                        title: item.title,
+                        image: item.image,
+                        author: item.author,
+                        publishDate: item.publishDate,
+                        isbn: item.isbn
+                    )
+                } as! T
+            )
+        } else if T.self == [BookSearchItem].self {
+            // todo - BookDetailInfoViewController
+        } else { print("request book search - 왜 다 타입이 안맞을까???") }
     }
 }

a0f0cab384b2c114012d32142de8e8ae188b2eb2
kimhyebeen <kimhbin@naver.com>
2021-01-20 01:56:55 +0900

    #8. 구현할 메서드 틀 잡기

--- BookStory/BookStory/View/BookCollectionCell.swift
+++ BookStory/BookStory/View/BookCollectionCell.swift
@@ -29,6 +29,27 @@ class BookCollectionCell: UICollectionViewCell {
         self.layer.shadowOpacity = 1
         self.layer.shadowColor = UIColor.black.cgColor
         self.layer.shadowOffset = CGSize(width: 0, height: 0)
+        
+        setupTitleLabel()
+        setupAuthorLabel()
+        setupPublishDateLabel()
+        setupImageView()
+    }
+    
+    private func setupTitleLabel() {
+        
+    }
+    
+    private func setupAuthorLabel() {
+        
+    }
+    
+    private func setupPublishDateLabel() {
+        
+    }
+    
+    private func setupImageView() {
+        
     }
 }
 

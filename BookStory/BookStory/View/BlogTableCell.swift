d7369b525fd5c6c522db91a3c46e726dac300017
kimhyebeen <kimhbin@naver.com>
2021-01-20 02:43:53 +0900

    #9. 블로그 제목 라벨 추가 및 구현

--- BookStory/BookStory/View/BlogTableCell.swift
+++ BookStory/BookStory/View/BlogTableCell.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class BlogTableCell: UITableViewCell {
     static let identifier = "BlogTableCell"
+    let title = UILabel()
+        .then {
+            $0.text = "블로그 제목"
+            $0.textColor = UIColor(named: "deep_gray")
+            $0.font = UIFont.boldSystemFont(ofSize: 18)
+        }
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -35,7 +41,12 @@ class BlogTableCell: UITableViewCell {
 
 extension BlogTableCell {
     private func setupTitleLabel() {
+        self.addSubview(title)
         
+        title.translatesAutoresizingMaskIntoConstraints = false
+        title.topAnchor.constraint(equalTo: self.topAnchor, constant: 8).isActive = true
+        title.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 12).isActive = true
+        title.trailingAnchor.constraint(lessThanOrEqualTo: self.trailingAnchor, constant: -12).isActive = true
     }
     
     private func setupPostDateLabel() {

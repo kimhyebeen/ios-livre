44d8fa3c3b4ca20e7519b65f952f0fa1d79eed8e
kimhyebeen <kimhbin@naver.com>
2021-01-19 19:17:14 +0900

    #7. Create a method for getting reward point

--- BookStory/BookStory/Detail/DetailViewModel.swift
+++ BookStory/BookStory/Detail/DetailViewModel.swift
@@ -22,6 +22,7 @@ class DetailViewModel {
         let booksResult = PublishRelay<[SimpleBookItem]>()
         let blogsResult = PublishRelay<[BlogItem]>()
         let newsResult = PublishRelay<[NewsItem]>()
+        let pointResult = BehaviorRelay<String>(value: "lv \(RewardConfig.getCurrentLevel()). \(Int(RewardConfig.getCurrentPoint()))/\(Int(RewardConfig.getPointList()![RewardConfig.getCurrentLevel()]))")
     }
     
     init() {
@@ -29,6 +30,7 @@ class DetailViewModel {
             .filter { !$0.isEmpty }
             .subscribe(onNext: { [weak self] text in
                 self?.requestAllSearchData(value: text)
+                self?.updatePoint(value: text)
             }).disposed(by: disposeBag)
     }
     
@@ -48,5 +50,14 @@ class DetailViewModel {
     
     private func requestNewsItems(value: String) {
         requestNews(query: value, relay: output.newsResult)
+    }
+    
+    private func updatePoint(value: String) {
+        RewardConfig.addPoint(point: Float(value.count * 2))
+        output.pointResult.accept(getPointString())
+    }
+    
+    private func getPointString() -> String {
+        return "lv \(RewardConfig.getCurrentLevel()). \(Int(RewardConfig.getCurrentPoint()))/\(Int(RewardConfig.getPointList()![RewardConfig.getCurrentLevel()]))"
     }
 }

96f49653dac7b1f342dbdf330f1c73d6dd37acb6
kimhyebeen <kimhbin@naver.com>
2021-01-20 16:14:38 +0900

    #15. 더보기 버튼 추가 및 구현

--- BookStory/BookStory/View/BlogField.swift
+++ BookStory/BookStory/View/BlogField.swift
@@ -38,8 +38,8 @@ class BlogField: UIView {
         self.backgroundColor = UIColor(named: "pale_gray")
         
         setupDivider()
-        setupMoreBlogButton()
         setupTitleLabel()
+        setupMoreBlogButton()
     }
     
     func setTableViewItem() {
@@ -63,12 +63,27 @@ extension BlogField {
         self.addSubview(title)
         
         title.translatesAutoresizingMaskIntoConstraints = false
-        title.centerYAnchor.constraint(equalTo: moreBlogButton.centerYAnchor).isActive = true
-        title.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8).isActive = true
+        title.centerYAnchor.constraint(equalTo: self.topAnchor, constant: 22.5).isActive = true
+        title.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
     }
     
     private func setupMoreBlogButton() {
+        setupMoreInformationButtonView()
+        self.addSubview(moreBlogButton)
         
+        moreBlogButton.translatesAutoresizingMaskIntoConstraints = false
+        moreBlogButton.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        moreBlogButton.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+    }
+    
+    private func setupMoreInformationButtonView() {
+        moreBlogButton.addSubview(moreInformationButtonView)
+        
+        moreInformationButtonView.translatesAutoresizingMaskIntoConstraints = false
+        moreInformationButtonView.topAnchor.constraint(equalTo: moreBlogButton.topAnchor, constant: 10).isActive = true
+        moreInformationButtonView.bottomAnchor.constraint(equalTo: moreBlogButton.bottomAnchor, constant: -10).isActive = true
+        moreInformationButtonView.leadingAnchor.constraint(equalTo: moreBlogButton.leadingAnchor).isActive = true
+        moreInformationButtonView.trailingAnchor.constraint(equalTo: moreBlogButton.trailingAnchor, constant: -10).isActive = true
     }
     
     private func setupBlogTableView() {

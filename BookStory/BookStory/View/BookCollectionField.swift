9ca74da88b160c9ac78071a63aa70dc3f4edc301
kimhyebeen <kimhbin@naver.com>
2021-01-20 20:13:39 +0900

    #11. BookCollectionField 구현 및 Paging 효과 적용

--- BookStory/BookStory/View/BookCollectionField.swift
+++ BookStory/BookStory/View/BookCollectionField.swift
@@ -8,6 +8,16 @@
 import UIKit
 
 class BookCollectionField: UIView {
+    var collectionView: UICollectionView!
+    let flowLayout = UICollectionViewFlowLayout()
+        .then {
+            $0.itemSize = CGSize(width: UIScreen.main.bounds.width * 0.6, height: 90)
+            $0.scrollDirection = .horizontal
+        }
+    
+    let spaceForLeftRight = (UIScreen.main.bounds.width * 0.9 - UIScreen.main.bounds.width * 0.6) / 2
+    var currentIndex: CGFloat = 0
+    var books: [SimpleBookItem] = []
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -22,13 +32,86 @@ class BookCollectionField: UIView {
     }
     
     private func setupView() {
-        
+        self.backgroundColor = UIColor.white.withAlphaComponent(0)
+        setupCollectionView()
+    }
+    
+    func setBookItems(items: [SimpleBookItem]) {
+        books = items
+        collectionView.reloadData()
     }
 
 }
 
 extension BookCollectionField {
     private func setupCollectionView() {
+        collectionView = UICollectionView(frame: self.frame, collectionViewLayout: flowLayout)
+            .then {
+                $0.register(BookCollectionCell.self, forCellWithReuseIdentifier: BookCollectionCell.identifier)
+                $0.backgroundColor = UIColor.white.withAlphaComponent(0)
+                $0.contentInset.left = spaceForLeftRight
+                $0.contentInset.right = spaceForLeftRight
+                $0.showsHorizontalScrollIndicator = false
+                $0.decelerationRate = .fast
+            }
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        self.addSubview(collectionView)
         
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+    }
+}
+
+extension BookCollectionField: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return books.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BookCollectionCell.identifier, for: indexPath) as? BookCollectionCell else {
+            return BookCollectionCell()
+        }
+        
+        cell.setBookInformation(item: books[indexPath.item])
+        return cell
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        // item의 사이즈와 item 간의 간격 사이즈를 구해서 하나의 item 크기로 설정.
+        guard let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else { return }
+        let cellWidthIncludingSpacing = layout.itemSize.width + layout.minimumLineSpacing
+        
+        // targetContentOff을 이용하여 x좌표가 얼마나 이동했는지 확인
+        // 이동한 x좌표 값과 item의 크기를 비교하여 몇 페이징이 될 것인지 값 설정
+        var offset = targetContentOffset.pointee
+        let index = (offset.x + scrollView.contentInset.left) / cellWidthIncludingSpacing
+        var roundedIndex = round(index)
+        
+        // scrollView, targetContentOffset의 좌표 값으로 스크롤 방향을 알 수 있다.
+        // index를 반올림하여 사용하면 item의 절반 사이즈만큼 스크롤을 해야 페이징이 된다.
+        // 스크로로 방향을 체크하여 올림,내림을 사용하면 좀 더 자연스러운 페이징 효과를 낼 수 있다.
+        if scrollView.contentOffset.x > targetContentOffset.pointee.x {
+            roundedIndex = floor(index)
+        } else if scrollView.contentOffset.x < targetContentOffset.pointee.x {
+            roundedIndex = ceil(index)
+        } else {
+            roundedIndex = round(index)
+        }
+
+        if currentIndex > roundedIndex && currentIndex != 0 {
+            currentIndex -= 1
+            roundedIndex = currentIndex
+        } else if currentIndex < roundedIndex {
+            currentIndex += 1
+            roundedIndex = currentIndex
+        }
+        
+        // 위 코드를 통해 페이징 될 좌표값을 targetContentOffset에 대입하면 된다.
+        offset = CGPoint(x: roundedIndex * cellWidthIncludingSpacing - spaceForLeftRight, y: -scrollView.contentInset.top)
+        targetContentOffset.pointee = offset
     }
 }

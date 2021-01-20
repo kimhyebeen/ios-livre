648804a4adffb64c0d8bb0df552cd63037af3c98
kimhyebeen <kimhbin@naver.com>
2021-01-20 13:14:10 +0900

    #15. BlogField 생성

---
+++ BookStory/BookStory/View/BlogField.swift
@@ -0,0 +1,28 @@
+//
+//  BlogField.swift
+//  BookStory
+//
+//  Created by 김혜빈 on 2021/01/20.
+//
+
+import UIKit
+
+class BlogField: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setupView()
+    }
+    
+    private func setupView() {
+        
+    }
+
+}

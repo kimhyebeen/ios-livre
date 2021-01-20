0cd0770d3cc57c45730fbeed622fea7cf31abb78
kimhyebeen <kimhbin@naver.com>
2021-01-20 16:28:42 +0900

    #15. BlogTableCell 레이아웃 수정

--- BookStory/BookStory/View/BlogTableCell.swift
+++ BookStory/BookStory/View/BlogTableCell.swift
@@ -26,7 +26,7 @@ class BlogTableCell: UITableViewCell {
             $0.text = "블로그 내용"
             $0.textColor = UIColor(named: "normal_gray")
             $0.font = UIFont.systemFont(ofSize: 14)
-            $0.numberOfLines = 0
+            $0.numberOfLines = 3
         }
     let bloggerName = UILabel()
         .then {
@@ -47,6 +47,7 @@ class BlogTableCell: UITableViewCell {
     
     private func setupView() {
         self.layer.cornerRadius = 10
+        self.backgroundColor = UIColor.white.withAlphaComponent(0)
         setupTitleLabel()
         setupPostDateLabel()
         setupDescriptionLabel()
@@ -67,35 +68,35 @@ extension BlogTableCell {
         
         title.translatesAutoresizingMaskIntoConstraints = false
         title.topAnchor.constraint(equalTo: self.topAnchor, constant: 8).isActive = true
-        title.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 12).isActive = true
-        title.trailingAnchor.constraint(lessThanOrEqualTo: self.trailingAnchor, constant: -12).isActive = true
+        title.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
+        title.trailingAnchor.constraint(lessThanOrEqualTo: self.trailingAnchor, constant: 15).isActive = true
     }
     
     private func setupPostDateLabel() {
         self.addSubview(postDate)
         
         postDate.translatesAutoresizingMaskIntoConstraints = false
-        postDate.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 6).isActive = true
-        postDate.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 12).isActive = true
-        postDate.trailingAnchor.constraint(lessThanOrEqualTo: self.trailingAnchor, constant: -12).isActive = true
+        postDate.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 2).isActive = true
+        postDate.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
+        postDate.trailingAnchor.constraint(lessThanOrEqualTo: self.trailingAnchor, constant: 15).isActive = true
     }
     
     private func setupDescriptionLabel() {
         self.addSubview(descriptionLabel)
         
         descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
-        descriptionLabel.topAnchor.constraint(equalTo: postDate.bottomAnchor, constant: 12).isActive = true
-        descriptionLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 12).isActive = true
-        descriptionLabel.trailingAnchor.constraint(lessThanOrEqualTo: self.trailingAnchor, constant: -12).isActive = true
+        descriptionLabel.topAnchor.constraint(equalTo: postDate.bottomAnchor, constant: 8).isActive = true
+        descriptionLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
+        descriptionLabel.trailingAnchor.constraint(lessThanOrEqualTo: self.trailingAnchor, constant: -15).isActive = true
     }
     
     private func setupBloggerNameLabel() {
         self.addSubview(bloggerName)
         
         bloggerName.translatesAutoresizingMaskIntoConstraints = false
-        bloggerName.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 12).isActive = true
-        bloggerName.leadingAnchor.constraint(greaterThanOrEqualTo: self.leadingAnchor, constant: 12).isActive = true
-        bloggerName.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -12).isActive = true
+        bloggerName.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 8).isActive = true
+        bloggerName.leadingAnchor.constraint(greaterThanOrEqualTo: self.leadingAnchor, constant: 15).isActive = true
+        bloggerName.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15).isActive = true
         bloggerName.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -8).isActive = true
     }
 }

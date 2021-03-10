f5cc504ad0625f3c2fe906854853398aaa70cc43
kimhyebeen <kimhbin@naver.com>
2021-03-10 11:14:02 +0900

    #28. 프로젝트 이름 변경 (Finder -> Livre) 및 gitignore 파일 수정

---
+++ Livre/livre/Data/SecretKeySet.swift
@@ -0,0 +1,16 @@
+//
+//  SecretKeySet.swift
+//  BookStory
+//
+//  Created by 김혜빈 on 2021/01/18.
+//
+
+import Foundation
+
+struct SecretKeySet {
+    static let keywordKey = "d0caf948-5c29-462d-9e65-286ef33f2123"
+    static let keywordServiceId = "00116013830"
+    
+    static let naverClientId = "LG4RJ0iA1WdXIDhpIOk5"
+    static let naverClientSecret = "MrVPnQRY7m"
+}

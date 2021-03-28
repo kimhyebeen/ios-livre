a4c57f1a8e241c1e2e3398982873988cf1844fa5
kimhyebeen <kimhbin@naver.com>
2021-03-28 13:47:14 +0900

    #34. Then pod 제거 및 SnapKit pod 추가

--- Livre/Pods/Target Support Files/SnapKit/SnapKit-umbrella.h
+++ Livre/Pods/Target Support Files/SnapKit/SnapKit-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double ThenVersionNumber;
-FOUNDATION_EXPORT const unsigned char ThenVersionString[];
+FOUNDATION_EXPORT double SnapKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char SnapKitVersionString[];
 

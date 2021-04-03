11bbbfa98549fc9e660d33b77a86c1b4b33352e6
kimhyebeen <kimhbin@naver.com>
2021-04-03 20:49:17 +0900

    #37. 데이터베이스 기능을 수행하는 PersistanceManager 클래스 생성 및 구현

---
+++ Livre/livre/Data/PersistenceManager.swift
@@ -0,0 +1,85 @@
+//
+//  RequestFavoriteBook.swift
+//  livre
+//
+//  Created by 김혜빈 on 2021/04/03.
+//
+
+import UIKit
+import CoreData
+
+class PersistenceManager {
+    
+    static var shared: PersistenceManager = PersistenceManager()
+    
+    private lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Favorite") // Favorite.xcdatamodeld의 PersistentContainer를 가져와요.
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        } )
+        
+        return container
+    }()
+    
+    private var context: NSManagedObjectContext { self.persistentContainer.viewContext }
+    private var entity: NSEntityDescription { NSEntityDescription.entity(forEntityName: "FavoriteBook", in: self.context)! }
+    
+    func insertBook(_ item: Book) -> Bool {
+        let book = NSManagedObject(entity: entity, insertInto: context)
+        book.setValue(item.titleString, forKey: "title")
+        book.setValue(item.image, forKey: "image")
+        book.setValue(item.authorString, forKey: "author")
+        book.setValue(item.contentsString, forKey: "contents")
+        book.setValue(item.publishDateString, forKey: "published")
+        
+        do {
+            try context.save()
+            return true
+        } catch {
+            print(error.localizedDescription)
+            return false
+        }
+    }
+    
+    func fetch<T: NSManagedObject>(request: NSFetchRequest<T>) -> [T] {
+        guard let result = try? self.context.fetch(request) else { return [] }
+        // 사용법: PersistenceManager.shared.fetch<FavoriteBook>(FavoriteBook.fetchRequest())
+        return result
+    }
+    
+    @discardableResult
+    func delete(object: NSManagedObject) -> Bool {
+        self.context.delete(object)
+        do {
+            try context.save()
+            return true
+        } catch {
+            print(error.localizedDescription)
+            return false
+        }
+    }
+    
+    @discardableResult
+    func deleteAll<T: NSManagedObject>(request: NSFetchRequest<T>) -> Bool {
+        let delete = NSBatchDeleteRequest(fetchRequest: T.fetchRequest())
+        do {
+            try self.context.execute(delete)
+            return true
+        } catch {
+            print(error.localizedDescription)
+            return false
+        }
+    }
+    
+    func count<T: NSManagedObject>(request: NSFetchRequest<T>) -> Int {
+        do {
+            let count = try self.context.count(for: request)
+            return count
+        } catch {
+            print(error.localizedDescription)
+            return -1
+        }
+    }
+}
